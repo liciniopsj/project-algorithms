e07aeb3b3640d42cf21b7ca1e69f5810de3f6dd8
liciniopsj <licinio.psj@gmail.com>
2023-06-09 01:17:30 -0300

    req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+
+    assert encrypt_message("secret", 3) == "ces_ter"
+    assert encrypt_message("secret", 4) == "te_rces"
+    assert encrypt_message("secret message", 9) == "em terces_egass"
+    assert encrypt_message("", 3) == ""
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("secret", "invalid_key")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 1)
